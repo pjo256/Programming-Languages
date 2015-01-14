01c9cb08492e7f076111ba2d6bdce12bb255ef05
pjo256 <pjo256@nyu.edu>
2015-01-14 15:50:27 +0100

    Starting a Rust roguelike

---
+++ Rust/dwemthys/src/main.rs
@@ -0,0 +1,19 @@
+extern crate tcod;
+use tcod::{Console, BackgroundFlag, KeyCode, Key};
+
+
+fn main() { 
+    let mut con = Console::init_root(80, 50, "libtcod Rust tutorial", false);
+    let mut exit = false;
+    while !(Console::window_closed() || exit) {
+        con.clear();
+        con.put_char(40, 25, '@', BackgroundFlag::Set);
+        Console::flush();
+        let keypress = Console::wait_for_keypress(true);
+        match keypress.key {
+            Key::Special(KeyCode::Escape) => exit = true,
+            _ => {}
+        }
+    } 
+}
+
