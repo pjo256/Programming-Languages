18beeb3ec01c8363cc77f90ee36090f49565217f
pjo256 <pjo256@nyu.edu>
2015-01-15 18:29:55 +0100

    Moved out rendering

--- Rust/dwemthys/src/main.rs
+++ Rust/dwemthys/src/main.rs
@@ -2,18 +2,26 @@ extern crate tcod;
 use tcod::{Console, BackgroundFlag, KeyCode, Key};
 
 
+fn render(con: &mut Console) {
+    con.clear();
+    con.put_char(40, 25, '@', BackgroundFlag::Set);
+    Console::flush();
+}
+
 fn main() { 
     let mut con = Console::init_root(80, 50, "libtcod Rust tutorial", false);
     let mut exit = false;
+    render(&mut con);
     while !(Console::window_closed() || exit) {
-        con.clear();
-        con.put_char(40, 25, '@', BackgroundFlag::Set);
-        Console::flush();
+        
         let keypress = Console::wait_for_keypress(true);
+
         match keypress.key {
             Key::Special(KeyCode::Escape) => exit = true,
             _ => {}
         }
+
+        render(&mut con);
     } 
 }
 
